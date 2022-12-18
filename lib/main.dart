f740b238e64f7cd57d1579a0662b19c249d5622e
shubham-indalkar <djsi55555@gmail.com>
2022-12-18 10:26:39 +0530

    reverted package name to fix crash and changed title of app bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PBS',
+      title: 'WePedL',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
