18d837f144e8be6b49787f105f43761f0f91c0ad
shubham-indalkar <djsi55555@gmail.com>
2022-12-21 21:17:24 +0530

    fixed animation again

--- lib/screens/loading/splash.dart
+++ lib/screens/loading/splash.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(milliseconds: 12680), () {
+    Timer(const Duration(milliseconds: 12600), () {
       setState(() {
         splashPrefixText = '';
       });
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
               // mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                SizedBox(width: (MediaQuery.of(context).size.width) / 2.5),
+                SizedBox(width: (MediaQuery.of(context).size.width) / 2.4),
                 Text(
                   splashPrefixText,
                   style: const TextStyle(
