c9027c7a06ab86eb09a7042ee2f7c140bf9a14b0
shubham-indalkar <djsi55555@gmail.com>
2022-12-23 15:48:52 +0530

    moved bottom bar and side bar to its own separate widget function

--- lib/screens/home/default_home.dart
+++ lib/screens/home/default_home.dart
@@ -59,7 +59,7 @@ class _DefaultHomeScreenState extends State<DefaultHomeScreen> {
     // Other logout logic goes here
   }
   // shared prefs //
-  
+
   @override
   void initState() {
     super.initState();
@@ -68,7 +68,6 @@ class _DefaultHomeScreenState extends State<DefaultHomeScreen> {
     getName();
     // shared pref //
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -79,154 +78,162 @@ class _DefaultHomeScreenState extends State<DefaultHomeScreen> {
         label: const Text("Unlock"),
         icon: const Icon(Icons.qr_code_scanner_outlined),
       ),
-      bottomNavigationBar: SalomonBottomBar(
-        currentIndex: _selectedIndex,
-        onTap: (i) => setState(() => _selectedIndex = i),
-        items: [
-          SalomonBottomBarItem(
-            icon: const Icon(Icons.home_outlined),
-            title: const Text("Home"),
-            selectedColor: Colors.blue,
-            activeIcon: const Icon(Icons.home),
-          ),
-          SalomonBottomBarItem(
-            icon: const Icon(Icons.account_balance_wallet_outlined),
-            title: const Text("Wallet"),
-            selectedColor: Colors.blue,
-            activeIcon: const Icon(Icons.account_balance_wallet),
-          ),
-          SalomonBottomBarItem(
-            icon: const Icon(Icons.help_center_outlined),
-            title: const Text("Get Help"),
-            selectedColor: Colors.blue,
-            activeIcon: const Icon(Icons.help_center),
-          ),
-          SalomonBottomBarItem(
-            icon: const Icon(Icons.person_outlined),
-            title: const Text("Profile"),
-            selectedColor: Colors.blue,
-            activeIcon: const Icon(Icons.person),
-          ),
-        ],
-      ),
+      bottomNavigationBar: _bottomBar(),
       appBar: AppBar(
         title: const Text('WePedL'),
         centerTitle: true,
       ),
-      drawer: SafeArea(
-        child: SidebarX(
-          showToggleButton: false,
-          animationDuration: const Duration(milliseconds: 0),
-          theme: const SidebarXTheme(
-            width: 220,
-          ),
-          headerBuilder: (context, extended) {
-            return SizedBox(
-              height: 190,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: CircleAvatar(
-                      radius: 60,
-                      backgroundImage: AssetImage(
-                          'assets/avatars/$_avatarIndex.png'), // shared prefs //
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5.0,
-                  ),
-                  // get name from firestore database and set here //
-                  Text(
-                    _name, // shared prefs //
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-          controller: SidebarXController(selectedIndex: 0, extended: true),
-          items: [
-            SidebarXItem(
-              icon: Icons.not_listed_location,
-              label: '  Ride History',
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const HistoryScreen(),
-                  ),
-                );
-              },
-            ),
-            SidebarXItem(
-              icon: Icons.group_add,
-              label: '  Refer & Earn',
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ReferScreen(),
-                  ),
-                );
-              },
-            ),
-            SidebarXItem(
-              icon: Icons.settings,
-              label: '  Settings',
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const SettingsScreen(),
-                  ),
-                );
-              },
-            ),
-          ],
-          footerBuilder: (context, extended) {
-            return SizedBox(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: TextButton.icon(
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                        const EdgeInsets.all(10.0)),
-                    backgroundColor:
-                        const MaterialStatePropertyAll<Color>(Colors.blue),
-                  ),
-                  onPressed: () {
-                    // logic for logout and disconnect from firebase //
-                    // shared prefs //
-                    logoutSharedPrefs();
-                    // shared prefs //
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  },
-                  icon: const Icon(
-                    Icons.exit_to_app,
-                    size: 18.0,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    "Logout",
-                    style: TextStyle(
-                      fontSize: 13.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+      drawer: _sideBar(),
       // logic for routing navigation bar tabs
       body: Center(
         child: _pages.elementAt(_selectedIndex),
       ),
+    );
+  }
+
+  Widget _sideBar() {
+    return SafeArea(
+      child: SidebarX(
+        showToggleButton: false,
+        animationDuration: const Duration(milliseconds: 0),
+        theme: const SidebarXTheme(
+          width: 220,
+        ),
+        headerBuilder: (context, extended) {
+          return SizedBox(
+            height: 190,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage(
+                        'assets/avatars/$_avatarIndex.png'), // shared prefs //
+                  ),
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                // get name from firestore database and set here //
+                Text(
+                  _name, // shared prefs //
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        controller: SidebarXController(selectedIndex: 0, extended: true),
+        items: [
+          SidebarXItem(
+            icon: Icons.not_listed_location,
+            label: '  Ride History',
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const HistoryScreen(),
+                ),
+              );
+            },
+          ),
+          SidebarXItem(
+            icon: Icons.group_add,
+            label: '  Refer & Earn',
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const ReferScreen(),
+                ),
+              );
+            },
+          ),
+          SidebarXItem(
+            icon: Icons.settings,
+            label: '  Settings',
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SettingsScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+        footerBuilder: (context, extended) {
+          return SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextButton.icon(
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                      const EdgeInsets.all(10.0)),
+                  backgroundColor:
+                      const MaterialStatePropertyAll<Color>(Colors.blue),
+                ),
+                onPressed: () {
+                  // logic for logout and disconnect from firebase //
+                  // shared prefs //
+                  logoutSharedPrefs();
+                  // shared prefs //
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.exit_to_app,
+                  size: 18.0,
+                  color: Colors.white,
+                ),
+                label: const Text(
+                  "Logout",
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _bottomBar() {
+    return SalomonBottomBar(
+      currentIndex: _selectedIndex,
+      onTap: (i) => setState(() => _selectedIndex = i),
+      items: [
+        SalomonBottomBarItem(
+          icon: const Icon(Icons.home_outlined),
+          title: const Text("Home"),
+          selectedColor: Colors.blue,
+          activeIcon: const Icon(Icons.home),
+        ),
+        SalomonBottomBarItem(
+          icon: const Icon(Icons.account_balance_wallet_outlined),
+          title: const Text("Wallet"),
+          selectedColor: Colors.blue,
+          activeIcon: const Icon(Icons.account_balance_wallet),
+        ),
+        SalomonBottomBarItem(
+          icon: const Icon(Icons.help_center_outlined),
+          title: const Text("Get Help"),
+          selectedColor: Colors.blue,
+          activeIcon: const Icon(Icons.help_center),
+        ),
+        SalomonBottomBarItem(
+          icon: const Icon(Icons.person_outlined),
+          title: const Text("Profile"),
+          selectedColor: Colors.blue,
+          activeIcon: const Icon(Icons.person),
+        ),
+      ],
     );
   }
 }
