3e65d04f78be66ead46c3e61f434ee2bbe0d334b
Shubham Indalkar <djsi55555@gmail.com>
2023-02-06 18:53:55 +0530

    Revert "feat fixed dark mode theme data"

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:public_bicycle_sharing/provider/theme.custom.dart';
 import 'package:public_bicycle_sharing/screens/home/default_home.dart';
 import 'package:public_bicycle_sharing/screens/home/qr_scan.dart';
 import 'package:public_bicycle_sharing/screens/loading/splash.dart';
 import 'package:public_bicycle_sharing/screens/settings/config.dart';
 import 'package:public_bicycle_sharing/services/shared_prefs.dart';
-import 'package:provider/provider.dart';
-
-import 'provider/theme.data.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,6 +17,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   SharedPrefGetsNSets sprefs = SharedPrefGetsNSets();
 
+  late String _themeColor;
+  late bool _isDark;
+
   Map<String, Color> stringToColor = {
     'blue': Colors.blue,
     'purple': Colors.purple,
@@ -29,27 +28,27 @@ class _MyAppState extends State<MyApp> {
     'brown': Colors.brown,
   };
 
-  // // shared pref //
-  // Future<void> getThemeColor() async {
-  //   String? themeColor = await sprefs.getThemeColor();
-  //   setState(() {
-  //     _themeColor = themeColor!;
-  //   });
-  // }
+  // shared pref //
+  Future<void> getThemeColor() async {
+    String? themeColor = await sprefs.getThemeColor();
+    setState(() {
+      _themeColor = themeColor!;
+    });
+  }
 
-  // // shared pref //
-  // Future<void> getDarkThemeStatus() async {
-  //   bool? isDark = await sprefs.getDarkThemeStatus();
-  //   setState(() {
-  //     _isDark = isDark!;
-  //   });
-  // }
+  // shared pref //
+  Future<void> getDarkThemeStatus() async {
+    bool? isDark = await sprefs.getDarkThemeStatus();
+    setState(() {
+      _isDark = isDark!;
+    });
+  }
 
   final ThemeData _lightTheme = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: Colors.white,
     brightness: Brightness.light,
-    primaryColor: Colors.blue, // #2196F3
+    primaryColor: Colors.blue,  // #2196F3
     // floatingActionButtonTheme:
     //     const FloatingActionButtonThemeData(backgroundColor: Colors.blue),
   );
@@ -74,19 +73,14 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'WePedL',
-        theme: CustomTheme.lightTheme(),
-        darkTheme: CustomTheme.darkTheme(),
-        themeMode: ThemeProvider().themeMode,
-        home: const SplashScreen(),
-        // home: const DefaultHomeScreen(), // for testing
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WePedL',
+      theme: _lightTheme,
+      darkTheme: _darkTheme,
+      themeMode: currentTheme.currentTheme(),
+      home: const SplashScreen(),
+      // home: const DefaultHomeScreen(), // for testing
     );
   }
 }
